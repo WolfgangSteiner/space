7fbcc6fb3eeffcd3340f26b80abf4b844020314e
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2020-12-25 21:29:16 +0100

    add ycm config

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,21 @@
+import os
+import ycm_core
+
+flags = [
+  '-Wall',
+  '-Wextra',
+  '-Werror',
+  '-Wno-long-long',
+  '-Wno-variadic-macros',
+  '-DNDEBUG',
+  '-std=c99',
+  '-Iunity/src',
+  ]
+
+SOURCE_EXTENSIONS = [ '.c', ]
+
+def FlagsForFile( filename, **kwargs ):
+  return {
+  'flags': flags,
+  'do_cache': True
+}
